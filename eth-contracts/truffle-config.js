93787d9afd452e24a4e6f077d9664b7c91fe6e25
Javi <contacto@scriptonita.com>
2020-06-17 21:54:56 +0200

    feat: deploy to Rinkeby network

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -45,6 +45,16 @@ module.exports = {
       host: "127.0.0.1", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*" // Any network (default: none)
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 4, // rinkeby's id
+      gas: 4500000, // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
     }
 
     // Another network with more advanced options...
